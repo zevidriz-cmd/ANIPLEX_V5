6acd241fc187ee32010c35f76b813eb9f105d632
Smart Habesha Developer <boy767674@gmail.com>
2026-07-19 20:37:05 +0300

    release: bump version to v1.5.6 (39)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.example.aniplex"
         minSdk = 26
         targetSdk = 36
-        versionCode = 38
-        versionName = "1.5.5"
+        versionCode = 39
+        versionName = "1.5.6"
         buildConfigField("String", "UPDATE_URL", "\"https://raw.githubusercontent.com/zevidriz-cmd/aniplex-releases/main/latest-version.json\"")
     }
 
